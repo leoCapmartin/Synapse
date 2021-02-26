ea26ffa0bed6e6d38d2e0887b61247b8f538ae83
olivier talane <olivier.talane@epita.fr>
2021-02-26 12:01:38 +0100

    ajout fichier manquant

---
+++ Synapse/Synapse/Assets/Scripts/Lobby.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Lobby : MonoBehaviourPunCallbacks
+{
+    public Text[] Player = new Text[2];
+    public Text RoomCode;
+
+    private void updatePlayerListUI()//pour afficher la liste des joueurs
+    {
+        int i = 0;
+        foreach (var kvp in PhotonNetwork.CurrentRoom.Players)
+        {
+            Player[i].text = kvp.Value.NickName;
+            i++;
+        }
+    }
+
+    // Start is called before the first frame update
+    public override void OnJoinedRoom()
+    {
+        RoomCode.text += PhotonNetwork.CurrentRoom.Name;
+        updatePlayerListUI();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Player[1].text = newPlayer.NickName;
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        if (otherPlayer.IsMasterClient)
+        {
+            Debug.Log("MasterClient left the room");
+            QuitRoom();
+        }
+        else
+        {
+            Debug.Log("Normal player left the room");
+            int i = 0;
+            foreach (var kvp in PhotonNetwork.CurrentRoom.Players)
+            {
+                Player[i].text = kvp.Value.NickName;
+                i++;
+            }
+        }
+    }
+
+    public void QuitRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        //updatePlayerListUI();
+        SceneManager.LoadScene("Menu");
+    }
+    public void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        {
+            SceneManager.LoadScene("Level");
+        }
+    }
+}
---
+++ Synapse/Synapse/Assets/Scripts/tmp/testobject.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testobject : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerCam;
+    public float throwForce = 500;
+
+    private bool hasPlayer = false;
+    private bool beingCarried = false;
+    private bool touched = false;
+
+    void Update()
+    {
+        // check distance entre objet et joueur
+        float dist = Vector3.Distance(gameObject.transform.position, player.position);
+
+        // si - ou = 1.9 unitÃ©s de distance = on peut ramasser
+        if (dist <= 3.5f)
+        {
+            hasPlayer = true;
+        }
+        else
+        {
+            hasPlayer = false;
+        }
+
+        // si on peut ramasser et qu'on appuie sur E = on porte l'objet
+        if (hasPlayer && Input.GetKey(KeyCode.E))
+        {
+            GetComponent< Rigidbody>().isKinematic = true;
+            transform.parent = playerCam;
+            beingCarried = true;
+        }
+
+        // Si on porte l'objet
+        if (beingCarried)
+        {
+            // si l'objet touche un mur / objet avec collider
+            if (touched)
+            {
+                GetComponent< Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                touched = false;
+            }
+
+            // Clique gauche = on jette l'objet
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent< Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                GetComponent< Rigidbody>().AddForce(playerCam.forward * throwForce);
+            }
+            // clique droit on pose l'objet
+            else if (Input.GetMouseButtonDown(1))
+            {
+                GetComponent< Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+            }
+        }
+    }
+
+    // DÃ©tection de contact grace au collider is trigger
+    void OnTriggerEnter()
+    {
+        if (beingCarried)
+        {
+            touched = true;
+        }
+    }
+}
