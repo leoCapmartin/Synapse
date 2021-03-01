7c39626737c314349b36a3f3c26d7e92fea844b1
olivier talane <olivier.talane@epita.fr>
2021-03-01 10:27:18 +0100

    ajout du menu pause et finissions menu multi

---
+++ Synapse/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused==true)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+
+    }
+    void Paused ()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        pauseMenuUI.SetActive(true);
+        gameIsPaused = true;
+    }
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        pauseMenuUI.SetActive(false);
+        gameIsPaused = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene("Menu");
+    }
+
+}
